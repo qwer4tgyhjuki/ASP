13e50bfbbc0185183d1bb00c13f5af8460ba4895
qwer4tgyhjuki <terokatop@gmail.com>
2024-01-21 17:43:42 +0300

    lr4

--- HelloWeb/Program.cs
+++ HelloWeb/Program.cs
@@ -1,19 +1,58 @@
 using HelloWeb;
 
-var builder = WebApplication.CreateBuilder();
-var app = builder.Build();
-Company company = new Company
-{
-    cName = "TD",
-    cDescription = "Description",
-    cEmail = "ttd211s@outlol.com",
-    cPhone = "+4172763821782",
-    cWorkerks = 50
-};
 
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+builder.Configuration.AddJsonFile("Configurations/Books.json");
+List<Book> books = builder.Configuration.GetSection("Books").Get<List<Book>>();
+builder.Configuration.AddJsonFile("Configurations/Profiles.json");
+List<Profile> profiles = builder.Configuration.GetSection("Profiles").Get<List<Profile>>();
 // Token Middlweare || 1g231
 app.UseMiddleware<TokenMiddleware>();
-app.MapGet("/", () => $"{company}");
-app.MapGet("/randnum", () => $"\nRandom number (0 to 100): {company.RandomNumber()}");
+app.Map("/", context =>
+{
+    context.Response.ContentType = "text/html";
+    return context.Response.WriteAsync(@"
+        <html>
+            <body>
+                <h1>Hello!</h1>
+            </body>
+        </html>
+    ");
+});
+app.Map("/Library", context =>
+{
+    context.Response.ContentType = "text/html";
+    return context.Response.WriteAsync(@"
+        <html>
+            <body>
+                <h1>Welcome to Library</h1>
+            </body>
+        </html>
+    ");
+});
+app.MapGet("Library/Books", async context =>
+{
+    string content = "";
+    foreach(var book in books)
+    {
+        content += $"{book.Id} - \"{book.Name}\" published by {book.Author} in {book.Year}\n";
+    }
+    await context.Response.WriteAsync(content);
+});
+app.Map("/Library/Profile/{id:int:range(0,5)?}", (string? id, HttpContext context) =>
+{
+    if (int.TryParse(id, out int userId))
+    {
+        var userProfile = profiles.FirstOrDefault(profile => profile.Id == userId);
+
+        if (userProfile != null)
+        {
+            return context.Response.WriteAsync($"User Id: {userProfile.Id}, Name: {userProfile.Name}");
+        }
+
+    }
+    return context.Response.WriteAsync($"User Id: {null}, Name: Guest");
+});
 app.Run();
 
